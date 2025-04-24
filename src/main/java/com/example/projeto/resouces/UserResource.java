11f193ebf6fb34f9747d8a63c74eb8e2cf27c177
Guiziin227 <gh63388@gmail.com>
2025-04-24 18:15:42 -0300

    feat: add UserResource class with endpoint to retrieve user data

---
+++ src/main/java/com/example/projeto/resouces/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.projeto.resouces;
+
+import com.example.projeto.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L,"malu","malu@gmail.com","12341234","malu123");
+        return ResponseEntity.ok().body(user);
+    }
+}
