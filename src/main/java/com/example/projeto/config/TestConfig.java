0455063fb5c96a887e3a0a35f52d1afbc15c2492
Guiziin227 <gh63388@gmail.com>
2025-04-25 17:14:53 -0300

    feat: implement Order entity, repository, service, and resource for order management

--- src/main/java/com/example/projeto/config/TestConfig.java
+++ src/main/java/com/example/projeto/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.example.projeto.config;
 
+import com.example.projeto.entities.Order;
 import com.example.projeto.entities.User;
+import com.example.projeto.repositories.OrderRepository;
 import com.example.projeto.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -15,6 +18,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
 
     @Override
@@ -25,5 +31,10 @@ public class TestConfig implements CommandLineRunner {
         User u3 = new User(null,"Malu","malu@gmail.com","12341234","malu123");
         userRepository.saveAll(Arrays.asList(u1, u2,u3));
 
+        Order o1 = new Order(null, Instant.parse("2025-03-12T12:02:23Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2025-02-12T13:04:23Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2025-01-15T15:05:33Z"), u1);
+        Order o4 = new Order(null, Instant.parse("2025-03-22T18:10:23Z"), u3);
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
     }
 }
