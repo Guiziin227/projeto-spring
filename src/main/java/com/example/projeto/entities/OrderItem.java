8aefa4ef2fb4aa291cb456a951095048f5ac2519
Guiziin227 <gh63388@gmail.com>
2025-04-27 16:47:51 -0300

    feat: add OrderItem and OrderItemPK entities for order-product association

---
+++ src/main/java/com/example/projeto/entities/OrderItem.java
@@ -0,0 +1,76 @@
+package com.example.projeto.entities;
+
+import com.example.projeto.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK orderItemPK;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        orderItemPK.setOrder(order);
+        orderItemPK.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return orderItemPK.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        orderItemPK.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return orderItemPK.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        orderItemPK.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(orderItemPK, orderItem.orderItemPK);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(orderItemPK);
+    }
+}
