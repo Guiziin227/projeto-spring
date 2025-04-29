1dcc92b4501653266c4e2334770ad7e762093dc1
Guiziin227 <gh63388@gmail.com>
2025-04-29 14:18:40 -0300

    feat: add exception handling for entity not found in UserService

--- src/main/java/com/example/projeto/services/UserService.java
+++ src/main/java/com/example/projeto/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.projeto.entities.User;
 import com.example.projeto.repositories.UserRepository;
 import com.example.projeto.services.exceptions.DatabaseException;
 import com.example.projeto.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -36,7 +37,7 @@ public class UserService {
             userRepository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
-        } catch (EmptyResultDataAccessException e) {
+        } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
         }
     }
@@ -48,8 +49,12 @@ public class UserService {
     }
 
     private void updateData(User entity, User user) {
-        entity.setName(user.getName());
-        entity.setEmail(user.getEmail());
-        entity.setPhoneNumber(user.getPhoneNumber());
+       try{
+           entity.setName(user.getName());
+           entity.setEmail(user.getEmail());
+           entity.setPhoneNumber(user.getPhoneNumber());
+       } catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(user.getId());
+       }
     }
 }
