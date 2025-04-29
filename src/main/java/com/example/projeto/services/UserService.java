fb46459d3a2969886e4289f442489819f7c4e230
Guiziin227 <gh63388@gmail.com>
2025-04-29 13:43:17 -0300

    feat: add exception handling for user not found in findById method

--- src/main/java/com/example/projeto/services/UserService.java
+++ src/main/java/com/example/projeto/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.projeto.services;
 
 import com.example.projeto.entities.User;
 import com.example.projeto.repositories.UserRepository;
+import com.example.projeto.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
